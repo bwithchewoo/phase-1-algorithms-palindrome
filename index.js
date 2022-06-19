6fb1199a6b40f534a020bf08046c5052973efecf
Anna Cho <loveablessing@gmail.com>
2022-06-19 01:30:30 -0400

    commit tree a6552fcf50dd69aa46a3e75e8e128c59a66cc009

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  wordReverse = [...word].reverse().join("")
+  if (wordReverse === word) {
+    return true
+  }else {
+    return false
+  }
 }
-
+isPalindrome("racecar")
 /* 
   Add your pseudocode here
-*/
+  function will receive one argument, a string.
+  turn the string into an array, reverse() the array and then join() it back into a string
+  then compare the new string to the initial string
+  */
 
 /*
   Add written explanation of your solution here
+  reverse the word and compare if the word is the same as before you reversed it, if it is, return true, else return false
 */
 
 // You can run `node index.js` to view these console logs
